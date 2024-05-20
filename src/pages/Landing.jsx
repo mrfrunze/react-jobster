d6f98914e8d09c16eb6fed180e038a3c3a0f5ae7
Ilya <stoynov.oleg@gmail.com>
2024-05-20 22:56:18 +0300

    commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,51 +1,13 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
-import styled from 'styled-components';
+import Wrapper from "../wrappers/LandingPage"
+import {Logo} from "../components";
 
-const Wrapper = styled.main`
-    nav{
-        width: var(--fluid-width);
-        max-width: var(--max-width);
-        margin: 0 auto;
-        height: var(--nav-height);
-        display: flex;
-        align-items: center;
-    }
-    .page{
-        min-height: calc(100vh - var(--nav-height));
-        display: grid;
-        align-items: center;
-    }
-    h1{
-        font-weight: 700;
-
-        span{
-            color: var(--primary-500);
-        }
-    }
-    p{
-        color: var(--gray-600);
-    }
-    .main-img{
-        display: none;
-    }
-    @media(min-width: 992px){
-        .page{
-            grid-template-columns: 1fr 1fr;
-            column-gap: 3rem;
-        }
-        .main-img{
-            display: block;
-        }
-    }
-
-`
 
 const Landing = () => {
     return (
         <Wrapper>
             <nav>
-                <img src={logo} alt="jobster logo" className="logo" />
+                <Logo/> <span>jobster</span>
             </nav>
             <div className="container page">
                 {/* info */}
