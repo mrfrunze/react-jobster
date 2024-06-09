ce6c96cfff44041c96303946fbba0f893c016d5b
Ilya <stoynov.oleg@gmail.com>
2024-06-09 13:22:59 +0300

    commit
    jobSlice.js

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
     <div className='form-row'>
-        <label htmlFor={name} className='form-label'>
-            {labelText || name}
-        </label>
-        <input
-            type={type}
-            value={value}
-            name={name}
-            onChange={handleChange}
-            className='form-input'
-        />
+      <label htmlFor={name} className='form-label'>
+        {labelText || name}
+      </label>
+      <input
+        id={name}
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className='form-input'
+      />
     </div>
   )
 }
