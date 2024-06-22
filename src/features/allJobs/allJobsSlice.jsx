d04da85a38074e2a24275e608c34919b84585108
Oleg <stoynov.oleg@gmail.com>
2024-06-22 11:47:33 +0300

    commit

--- src/features/allJobs/allJobsSlice.jsx
+++ src/features/allJobs/allJobsSlice.jsx
@@ -66,6 +66,9 @@ const allJobsSlice = createSlice({
         clearFilters: (state) => {
             return { ...state, ...initialFiltersState };
         },
+        changePage: (state, { payload }) => {
+            state.page = payload;
+        },
     },
     extraReducers: {
         [getAllJobs.pending]: (state) => {
@@ -74,6 +77,8 @@ const allJobsSlice = createSlice({
         [getAllJobs.fulfilled]: (state, { payload }) => {
             state.isLoading = false;
             state.jobs = payload.jobs;
+            state.numOfPages = payload.numOfPages;
+            state.totalJobs = payload.totalJobs;
         },
         [getAllJobs.rejected]: (state, { payload }) => {
             state.isLoading = false;
@@ -94,6 +99,6 @@ const allJobsSlice = createSlice({
     }
 });
 
-export const { showLoading, hideLoading, handleChange, clearFilters} = allJobsSlice.actions;
+export const { showLoading, hideLoading, handleChange, clearFilters, changePage} = allJobsSlice.actions;
 
 export default allJobsSlice.reducer;
