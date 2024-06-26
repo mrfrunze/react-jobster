49642297926ab2278814fa8b34e02e84a3a6b2be
Oleg <stoynov.oleg@gmail.com>
2024-06-26 18:31:19 +0300

    UnAuthorized Requests

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,9 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
-import customFetch from '../../utils/axios';
 import { getUserFromLocalStorage } from '../../utils/localStorage';
-import { logoutUser } from '../user/userSlice';
-import { showLoading, hideLoading, getAllJobs } from '../allJobs/allJobsSlice';
 import { createJobThunk, deleteJobThunk, editJobThunk } from './jobThunk';
 
 
--- src/features/user/userThunk.js
+++ src/features/user/userThunk.js
@@ -1,4 +1,4 @@
-import customFetch from '../../utils/axios';
+import customFetch, {checkForUnauthorizedResponse} from '../../utils/axios';
 import { clearAllJobsState } from '../allJobs/allJobsSlice';
 import { logoutUser } from './userSlice';
 import { clearValues } from '../job/jobSlice';
@@ -9,7 +9,7 @@ export const registerUserThunk = async (url, user, thunkAPI) => {
         const resp = await customFetch.post(url, user);
         return resp.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue(error.response.data.msg);
+        return checkForUnauthorizedResponse(error, thunkAPI);
     }
 }
 
@@ -18,7 +18,7 @@ export const loginUserThunk = async (url, user, thunkAPI) => {
         const resp = await customFetch.post(url, user);
         return resp.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue(error.response.data.msg);
+        return checkForUnauthorizedResponse(error, thunkAPI);
     }
 };
 
@@ -32,7 +32,7 @@ export const updateUserThunk = async (url, user, thunkAPI) => {
             thunkAPI.dispatch(logoutUser());
             return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
         }
-        return thunkAPI.rejectWithValue(error.response.data.msg);
+        return checkForUnauthorizedResponse(error, thunkAPI);
     }
 };
 
