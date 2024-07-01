d32bd68e36781941ea4b20cbd49df08692316a5e
Oleg <stoynov.oleg@gmail.com>
2024-07-01 18:16:27 +0300

    vite.config.js
    Указываем Vite создавать папку build вместо dist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Указываем Vite создавать папку build вместо dist
+  }
 })
